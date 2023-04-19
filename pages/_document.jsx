6f9c94eda3588bc02005830e3e1ff4acd24d3a33
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-04-19 15:53:03 -0700

    favicon and old imgs deleted

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,7 @@ export default function Document() {
             <meta property="og:title" content="Bridger Brown Dev"/>
             <meta property="og:image" content={"/bbdev-meta.jpg"}/>
             <meta property="og:description" content="Hello, my name is Bridger Brown and I am a Front-End Web developer and designer based out of Portland, Oregon."/>
-            <link rel="icon" type="image/x-icon" href="/b-favicon.ico"/>
+            <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
             <link rel="stylesheet" href="https://use.typekit.net/ugk8hvo.css"/>
             <Script src="https://kit.fontawesome.com/86ad898daa.js" crossOrigin="anonymous"></Script>
