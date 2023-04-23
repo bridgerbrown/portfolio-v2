97fdf7a891747f16516139365df83bc1e7322f69
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-04-23 16:32:29 -0700

    Promise-based texture loading, brighter bg

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,29 +19,25 @@ function Home() {
 
   useEffect(() => {
       const scene = new THREE.Scene();
-      scene.backgroundIntensity = 1.4;
-        
-      new TextureLoader().load( 'sky-orange-contrast.png', function ( texture ) {
-        texture.mapping = THREE.EquirectangularReflectionMapping;
-        scene.background = texture;
-        scene.environment = texture;
-        renderer.render(scene, camera);
-      });
-
-      let camera
-      if (isMobile) {
-        camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000, );
-        camera.position.setX(60);
-        camera.position.setY(60);
-        camera.position.setZ(0);
-        camera.lookAt(0, 0, 0);
-      } else {
-        camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000, );
-        camera.position.setX(60);
-        camera.position.setY(60);
-        camera.position.setZ(0);
-        camera.lookAt(0, 0, 0);
-      }
+      scene.backgroundIntensity = 1.2;
+      
+      let camera = isMobile
+        ? new THREE.PerspectiveCamera(
+            80,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+          )
+        : new THREE.PerspectiveCamera(
+            50,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+          );
+      camera.position.setX(60);
+      camera.position.setY(60); 
+      camera.position.setZ(0);
+      camera.lookAt(0, 0, 0);
 
       const renderer = new THREE.WebGLRenderer({
         canvas: document.querySelector('#canvasBg'),
@@ -49,8 +45,31 @@ function Home() {
       })
       renderer.setPixelRatio(window.devicePixelRatio);
       renderer.setSize(window.innerWidth, window.innerHeight);
-
       window.addEventListener( 'resize', onWindowResized );
+
+      function updateIsMobile() {
+        setIsMobile(window.innerWidth < 800)
+      }
+      updateIsMobile()
+  
+      function onWindowResized() {
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        if(isMobile){
+          camera.aspect = window.innerWidth / window.innerHeight;
+          camera.updateProjectionMatrix();
+        }
+        updateIsMobile()
+      }
+
+      // const textureLoader = new TextureLoader();
+      // textureLoader.load( 'sky-orange-contrast.png', function (texture){
+      //   texture.mapping = THREE.EquirectangularReflectionMapping;
+      //   scene.background = texture;
+      //   scene.environment = texture;
+      //   renderer.render(scene, camera);
+      // });
   
       const controls = new OrbitControls(camera, renderer.domElement);
   
@@ -69,116 +88,109 @@ function Home() {
         roughness: 0,
         metalness: 1.1,
       } );
-      
-      const orbitingSpheres = [];
-      for (let i = 0; i < 3; i++) {
-        const sphere = new THREE.Mesh(new THREE.IcosahedronGeometry( 15, 8 ), material );
-        orbitingSpheres.push(sphere);
-        centerSphere.add(sphere);
-        // Position the sphere around the center sphere
-        const angle = (2 * Math.PI * i) / 3;
-        sphere.position.set(15 * Math.sin(angle), 0, 15 * Math.cos(angle));
-      }
-  
-      const pointLight = new THREE.PointLight(0xffffff, 1);
-      pointLight.position.set(-50, 70, 0);
-      scene.add(pointLight);
-  
-      function lerp(x, y, a){
-        return (1 - a) * x + a * y
-      }
-  
-      function scalePercent(start, end) {
-        return (scrollPercent - start) / (end - start)
-      }
-    
-      const animationScripts = []
-        animationScripts.push({
-          start: 0,
-          end: 40,
-          func: () => {
-            const distance = 0 + window.scrollY / 60;
-      
-            orbitingSpheres[0].position.set(distance * Math.sin((2 * Math.PI * 0) / 3), 0, distance * Math.cos((2 * Math.PI * 0) / 3));
-            orbitingSpheres[1].position.set(distance * Math.sin((2 * Math.PI * 1) / 3), 0, distance * Math.cos((2 * Math.PI * 1) / 3));
-            orbitingSpheres[2].position.set(distance * Math.sin((2 * Math.PI * 2) / 3), 0, distance * Math.cos((2 * Math.PI * 2) / 3));
-  
-            camera.position.x = lerp(30, 50, scalePercent(0, 40));
-            camera.position.y = lerp(30, 70, scalePercent(0, 40));
-            camera.position.z = lerp(40, 40, scalePercent(0, 40));
-  
-            centerSphere.rotation.x = lerp(6, 9, scalePercent(0, 40));
-          },
+
+      function loadTexture(url) {
+        return new Promise(resolve => {
+          new THREE.TextureLoader().load(url, resolve)
         })
-        animationScripts.push({
-          start: 40,
-          end: 101,
-          func: () => {
-            const stoppingDistance = 25
-            let distance = (0 + window.scrollY / 60) * (1 - scalePercent(40, 101))
-            if (distance < stoppingDistance) {
-              distance = stoppingDistance
-            }
-  
-            orbitingSpheres[0].position.set(distance * Math.sin((2 * Math.PI * 0) / 3), 0, distance * Math.cos((2 * Math.PI * 0) / 3));
-            orbitingSpheres[1].position.set(distance * Math.sin((2 * Math.PI * 1) / 3), 0, distance * Math.cos((2 * Math.PI * 1) / 3));
-            orbitingSpheres[2].position.set(distance * Math.sin((2 * Math.PI * 2) / 3), 0, distance * Math.cos((2 * Math.PI * 2) / 3));
-  
-            camera.position.x = lerp(50, 40, scalePercent(40, 85));
-            camera.position.y = lerp(70, 10, scalePercent(40, 85));
-  
-            centerSphere.rotation.x = lerp(9, 12, scalePercent(40, 101));
-          },
-        })
+      }
       
-  
-        function playScrollAnimations() {
-          animationScripts.forEach((a) => {
-              if (scrollPercent >= a.start && scrollPercent < a.end) {
-                  a.func()
-              }
-          })
+      loadTexture('sky-orange-contrast.png').then((texture) => {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        scene.background = texture;
+        scene.environment = texture;
+        renderer.render(scene, camera);
+
+        const orbitingSpheres = [];
+        for (let i = 0; i < 3; i++) {
+          const sphere = new THREE.Mesh(new THREE.IcosahedronGeometry( 15, 8 ), material );
+          orbitingSpheres.push(sphere);
+          centerSphere.add(sphere);
+          // Position the sphere around the center sphere
+          const angle = (2 * Math.PI * i) / 3;
+          sphere.position.set(15 * Math.sin(angle), 0, 15 * Math.cos(angle));
         }
     
-        let scrollPercent = 0
+        const pointLight = new THREE.PointLight(0xffffff, 1);
+        pointLight.position.set(-50, 70, 0);
+        scene.add(pointLight);
+    
+        function lerp(x, y, a){
+          return (1 - a) * x + a * y
+        }
+    
+        function scalePercent(start, end) {
+          return (scrollPercent - start) / (end - start)
+        }
       
-      document.body.onscroll = () => {
+        const animationScripts = []
+          animationScripts.push({
+            start: 0,
+            end: 40,
+            func: () => {
+              const distance = 0 + window.scrollY / 60;
+        
+              orbitingSpheres[0].position.set(distance * Math.sin((2 * Math.PI * 0) / 3), 0, distance * Math.cos((2 * Math.PI * 0) / 3));
+              orbitingSpheres[1].position.set(distance * Math.sin((2 * Math.PI * 1) / 3), 0, distance * Math.cos((2 * Math.PI * 1) / 3));
+              orbitingSpheres[2].position.set(distance * Math.sin((2 * Math.PI * 2) / 3), 0, distance * Math.cos((2 * Math.PI * 2) / 3));
+    
+              camera.position.x = lerp(30, 50, scalePercent(0, 40));
+              camera.position.y = lerp(30, 70, scalePercent(0, 40));
+              camera.position.z = lerp(40, 40, scalePercent(0, 40));
+    
+              centerSphere.rotation.x = lerp(6, 9, scalePercent(0, 40));
+            },
+          })
+          animationScripts.push({
+            start: 40,
+            end: 101,
+            func: () => {
+              const stoppingDistance = 25
+              let distance = (0 + window.scrollY / 60) * (1 - scalePercent(40, 101))
+              if (distance < stoppingDistance) {
+                distance = stoppingDistance
+              }
+    
+              orbitingSpheres[0].position.set(distance * Math.sin((2 * Math.PI * 0) / 3), 0, distance * Math.cos((2 * Math.PI * 0) / 3));
+              orbitingSpheres[1].position.set(distance * Math.sin((2 * Math.PI * 1) / 3), 0, distance * Math.cos((2 * Math.PI * 1) / 3));
+              orbitingSpheres[2].position.set(distance * Math.sin((2 * Math.PI * 2) / 3), 0, distance * Math.cos((2 * Math.PI * 2) / 3));
+    
+              camera.position.x = lerp(50, 40, scalePercent(40, 85));
+              camera.position.y = lerp(70, 10, scalePercent(40, 85));
+    
+              centerSphere.rotation.x = lerp(9, 12, scalePercent(40, 101));
+            },
+          })
+        
+    
+          function playScrollAnimations() {
+            animationScripts.forEach((a) => {
+                if (scrollPercent >= a.start && scrollPercent < a.end) {
+                    a.func()
+                }
+            })
+          }
+      
+          let scrollPercent = 0
+        
+        document.body.onscroll = () => {
           scrollPercent =
               ((document.documentElement.scrollTop || document.body.scrollTop) /
                   ((document.documentElement.scrollHeight ||
                       document.body.scrollHeight) -
                       document.documentElement.clientHeight)) *
               100
-          // ;(document.getElementById('scrollProgress')).innerText =
-          //     'Scroll Progress : ' + scrollPercent.toFixed(2)
-      }
-    
-  
-      function onWindowResized() {
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-        if(isMobile){
-          camera.aspect = window.innerWidth / window.innerHeight;
-          camera.updateProjectionMatrix();
         }
-        updateIsMobile()
-      }
-
-      function updateIsMobile() {
-        setIsMobile(window.innerWidth < 800)
-      }
-
-      updateIsMobile()
-  
-      function animate() {
-        cubeCamera.update( renderer, scene );
-        requestAnimationFrame(animate);
-        playScrollAnimations()
-        controls.update();
-        renderer.render(scene, camera);
-      }
-      animate();
+    
+        function animate() {
+          cubeCamera.update( renderer, scene );
+          requestAnimationFrame(animate);
+          playScrollAnimations()
+          controls.update();
+          renderer.render(scene, camera);
+        }
+        animate();
+      })
     }, [isMobile])
 
   const refScrollUp = useRef();
