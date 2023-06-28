02cb04ff3b2191d617d803aa226b550cac02d715
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-06-28 13:37:02 -0700

    little styling fixes

--- components/sections/Landing.jsx
+++ components/sections/Landing.jsx
@@ -8,7 +8,7 @@ function Landing() {
             <div className='Landing-box'>
                 <div className='Landing-Text'>
                     <h1>Bridger Brown</h1>
-                    <h2>Front-End Web Developer + Designer</h2>
+                    <h2>Front-End Web Developer</h2>
                 </div>
                 <div className='landpages'>
                 </div>
